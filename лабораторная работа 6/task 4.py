681a360f4c03be58e3c10994cd6c3fb981d1bed0
vyacheslavzajtsev <115566689+vyacheslavzajtsev@users.noreply.github.com>
2022-11-30 18:35:09 +0300

    Add files via upload

---
+++ лабораторная работа 6/task 4.py
@@ -0,0 +1,24 @@
+import json
+
+INPUT_FILE = "input.csv"
+
+
+def csv_to_list_dict(filename: str,
+                     delimeter: str = ',',
+                     new_line: str = '\n'
+                     ) -> list[dict]:
+    # TODO реализовать конвертер из csv в json
+    with open(filename) as a:
+        f = []
+        for q, l in enumerate(a):
+            k = l.strip(new_line).split(delimeter)
+            if q == 0:
+                z = k
+                continue
+            f.append({})
+            for i, _ in enumerate(z):
+                f[-1][z[i]] = k[i]
+    return f
+
+
+print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
