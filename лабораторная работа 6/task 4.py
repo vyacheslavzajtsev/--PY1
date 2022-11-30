a7578efeb87e547d39fa5abb65918e1f750b5c98
vyacheslavzajtsev <115566689+vyacheslavzajtsev@users.noreply.github.com>
2022-11-30 18:46:45 +0300

    Лабораторная работа 6

--- лабораторная работа 6/task 4.py
+++ лабораторная работа 6/task 4.py
@@ -7,18 +7,19 @@ def csv_to_list_dict(filename: str,
                      delimeter: str = ',',
                      new_line: str = '\n'
                      ) -> list[dict]:
-    # TODO реализовать конвертер из csv в json
+    # TODO СЂРµР°Р»РёР·РѕРІР°С‚СЊ РєРѕРЅРІРµСЂС‚РµСЂ РёР· csv РІ json
     with open(filename) as a:
-        f = []
+        fl = []
         for q, l in enumerate(a):
             k = l.strip(new_line).split(delimeter)
             if q == 0:
                 z = k
                 continue
-            f.append({})
+            fl.append({})
             for i, _ in enumerate(z):
-                f[-1][z[i]] = k[i]
-    return f
+                fl[-1][z[i]] = k[i]
+
+    return fl
 
 
 print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
